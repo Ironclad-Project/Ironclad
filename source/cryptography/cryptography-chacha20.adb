7de0996797afaaf3ea36edb440076e3ff62153ce
streaksu <streaksu@protonmail.com>
2022-10-04 16:41:17 +0200

    cryptography: Add a missing last pass to chacha20

--- source/cryptography/cryptography-chacha20.adb
+++ source/cryptography/cryptography-chacha20.adb
@@ -24,13 +24,14 @@ package body Cryptography.Chacha20 is
       UN : constant Unsigned_64 := Shift_Right (Nonce, 32)    and 16#FFFFFFFF#;
       LN : constant Unsigned_64 := Shift_Right (Nonce, 0)     and 16#FFFFFFFF#;
 
-      B : Block := (
+      Initial_Block : constant Block := (
          --  Hardcoded values of "expa", "nd 3", "2-by", "te k"
              16#65787061#,     16#6E642065#,     16#322D6279#,    16#7465206B#,
                  K.Value1,         K.Value2,         K.Value3,        K.Value4,
                  K.Value5,         K.Value6,         K.Value7,        K.Value8,
          Unsigned_32 (UB), Unsigned_32 (LB), Unsigned_32 (UN), Unsigned_32 (LN)
       );
+      B : Block := Initial_Block;
    begin
       --  Do 2 iterations at once.
       for I in 1 .. 20 / 2 loop
@@ -47,6 +48,10 @@ package body Cryptography.Chacha20 is
          Quarter_Round (B (3), B (4), B  (9), B (14));
       end loop;
 
+      --  Sum the initial and final.
+      for I in B'Range loop
+         B (I) := B (I) + Initial_Block (I);
+      end loop;
       return B;
    end Gen_Key;
 
