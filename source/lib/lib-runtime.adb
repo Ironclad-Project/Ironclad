7dd56ea41d72443eb18ad0ec77502cbdc2f799a1
streaksu <streaksu@mailbox.org>
2024-12-30 18:41:50 +0100

    lib: move mem* family functions to architectural ASM
    performance!!!
    
    Signed-off-by: streaksu <streaksu@mailbox.org>

--- source/lib/lib-runtime.adb
+++ source/lib/lib-runtime.adb
@@ -16,8 +16,7 @@
 
 with Lib.Messages;
 with Lib.Panic;
-with System.Storage_Elements; use System.Storage_Elements;
-with Interfaces;              use Interfaces;
+with Interfaces; use Interfaces;
 
 package body Lib.Runtime with SPARK_Mode => Off is
    --  Failing a check here would just make it infinitely recursive.
@@ -34,68 +33,4 @@ package body Lib.Runtime with SPARK_Mode => Off is
          ("Ada exception triggered at " & File_String & ":" &
           Line_Buffer (Line_Buffer'Last - Line_Len + 1 .. Line_Buffer'Last));
    end Last_Chance_Handler;
-   ----------------------------------------------------------------------------
-   function MemCmp (S1, S2 : System.Address; Size : size_t) return int is
-      Str1 : array (1 .. Size) of Unsigned_8 with Address => S1, Import;
-      Str2 : array (1 .. Size) of Unsigned_8 with Address => S2, Import;
-   begin
-      for I in 1 .. Size loop
-         if Str1 (I) /= Str2 (I) then
-            if Str1 (I) < Str2 (I) then
-               return -1;
-            else
-               return 1;
-            end if;
-         end if;
-      end loop;
-      return 0;
-   end MemCmp;
-
-   function MemCpy
-      (Desto, Source : System.Address;
-       Size          : size_t) return System.Address
-   is
-      Dst : array (1 .. Size) of Unsigned_8 with Address => Desto,  Import;
-      Src : array (1 .. Size) of Unsigned_8 with Address => Source, Import;
-   begin
-      for I in 1 .. Size loop
-         Dst (I) := Src (I);
-      end loop;
-      return Desto;
-   end MemCpy;
-
-   function MemMove
-      (Desto, Source : System.Address;
-       Size          : size_t) return System.Address
-   is
-      Dst : array (1 .. Size) of Unsigned_8 with Address => Desto,  Import;
-      Src : array (1 .. Size) of Unsigned_8 with Address => Source, Import;
-      Desto_Int  : constant Integer_Address := To_Integer (Desto);
-      Source_Int : constant Integer_Address := To_Integer (Source);
-   begin
-      if Source_Int > Desto_Int then
-         for I in Dst'Range loop
-            Dst (I) := Src (I);
-         end loop;
-      elsif Source_Int < Desto_Int then
-         for I in reverse Dst'Range loop
-            Dst (I) := Src (I);
-         end loop;
-      end if;
-      return Desto;
-   end MemMove;
-
-   function MemSet
-      (Desto : System.Address;
-       Value : Integer;
-       Size  : size_t) return System.Address
-   is
-      Val : constant Unsigned_8 := Unsigned_8 (Value);
-      Dst : array (1 .. Size) of Unsigned_8 with Address => Desto, Import;
-   begin
-      for C of Dst loop
-         C := Val;
-      end loop;
-      return Desto;
-   end MemSet;
 end Lib.Runtime;
