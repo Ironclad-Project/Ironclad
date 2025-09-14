79bcda48e12a8067daf905ccf953efb9dd5b3b63
streaksu <streaksu@mailbox.org>
2025-09-14 17:58:59 +0200

    Revert "memory: use more appropiate locking for MMU routines"
    This reverts commit 70ca75acdf20d6cf8d30b91c8c09c91ab06b4a1c.
    
    Reverted because this commit inadvertedly deadlocks Ironclad when
    running on 1 CPU under Gloire.

--- source/memory/memory-mmu.ads
+++ source/memory/memory-mmu.ads
@@ -199,7 +199,7 @@ private
    type PML4_Acc is access PML4;
    type Page_Table is record
       PML4_Level : PML4;
-      Mutex      : aliased Synchronization.Binary_Semaphore;
+      Mutex      : aliased Synchronization.Readers_Writer_Lock;
       User_Size  : Unsigned_64;
    end record;
 
